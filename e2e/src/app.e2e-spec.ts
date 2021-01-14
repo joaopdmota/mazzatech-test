a48c1520c33346aba4067ff0659fe5ca2b28b2ea
joaopdmota <jp.1998.jpd@icloud.com>
2021-01-14 12:31:53 -0300

    finishing project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to meu-primeiro-app!');
+    expect(page.getTitleText()).toEqual('Welcome to mazzatech-test!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meu-primeiro-app'`, () => {
+  it(`should have as title 'mazzatech-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('meu-primeiro-app');
+    expect(app.title).toEqual('mazzatech-test');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to meu-primeiro-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mazzatech-test!');
   });
 });
